2fe7365062b21ab23e8778a59ded766cae3fcb22
John Axel Eriksson <john@insane.se>
2019-06-11 14:10:28 +0200

    Some fixes.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use exitfailure::ExitFailure;
+use failure::Error;
 use i3ipc::{
     event::{inner::WindowChange, Event},
+    reply::Command,
     I3Connection, I3EventListener, Subscription,
 };
 use signal_hook::{iterator::Signals, SIGHUP, SIGINT, SIGQUIT, SIGTERM};
@@ -10,24 +12,24 @@ use structopt::StructOpt;
 #[derive(StructOpt)]
 struct Cli {
     #[structopt(short = "o", long = "opacity", default_value = "0.78")]
-    opacity: f32,
+    opacity: f64,
 }
 
-fn update_opacity(ipc: &mut I3Connection, opacity: f32) {
+fn update_opacity(ipc: &mut I3Connection, opacity: f64) -> Result<Command, Error> {
     let cmd = format!("[tiling] opacity {}; opacity 1", opacity);
-    ipc.run_command(&cmd).unwrap();
+    Ok(ipc.run_command(&cmd)?)
 }
 
-fn reset_opacity(ipc: &mut I3Connection) {
-    ipc.run_command("[tiling] opacity 1").unwrap();
+fn reset_opacity(ipc: &mut I3Connection) -> Result<Command, Error> {
+    Ok(ipc.run_command("[tiling] opacity 1")?)
 }
 
-fn handle_signals() -> Result<(), ExitFailure> {
-    let mut conn = I3Connection::connect().unwrap();
-    let signals = Signals::new(&[SIGHUP, SIGINT, SIGQUIT, SIGTERM]).unwrap();
+fn handle_signals() -> Result<(), Error> {
+    let mut conn = I3Connection::connect()?;
+    let signals = Signals::new(&[SIGHUP, SIGINT, SIGQUIT, SIGTERM])?;
     signals.forever().next();
-    reset_opacity(&mut conn);
-    exit(0);
+    reset_opacity(&mut conn)?;
+    exit(0)
 }
 
 fn main() -> Result<(), ExitFailure> {
@@ -37,12 +39,12 @@ fn main() -> Result<(), ExitFailure> {
     let mut listener = I3EventListener::connect()?;
     listener.subscribe(&[Subscription::Window])?;
 
-    reset_opacity(&mut conn);
+    reset_opacity(&mut conn)?;
     for event in listener.listen() {
         match event? {
             Event::WindowEvent(info) => {
                 if let WindowChange::Focus = info.change {
-                    update_opacity(&mut conn, args.opacity);
+                    update_opacity(&mut conn, args.opacity)?;
                 }
             }
             _ => unreachable!(),
