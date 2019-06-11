a1ff36781656ef012c56b80aa23cf0901201cab5
John Axel Eriksson <john@insane.se>
2019-06-11 14:13:05 +0200

    Make shell.nix self-contained

--- shell.nix
+++ shell.nix
@@ -1,6 +1,15 @@
-with import <nixpkgs> {};
+let
+  mozilla = import (builtins.fetchGit {
+      url = "https://github.com/mozilla/nixpkgs-mozilla.git";
+      ref = "master";
+      rev = "9f35c4b09fd44a77227e79ff0c1b4b6a69dff533";
+  });
+in
+  with import <nixpkgs> (
+    { overlays = [ mozilla ]; }
+  );
 
-mkShell {
- buildInputs = [ latest.rustChannels.stable.rust rustracer ];
- RUST_SRC_PATH = "${latest.rustChannels.stable.rust-src}/lib/rustlib/src/rust/src";
-}
+  mkShell {
+   buildInputs = [ latest.rustChannels.stable.rust rustracer ];
+   RUST_SRC_PATH = "${latest.rustChannels.stable.rust-src}/lib/rustlib/src/rust/src";
+  }
