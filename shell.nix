269d8a25267086ae05c9347f7a5a427a26d753ac
John Axel Eriksson <john@insane.se>
2019-07-18 23:17:38 +0200

    Improve shell.nix somewhat.

--- shell.nix
+++ shell.nix
@@ -4,12 +4,14 @@ let
       ref = "master";
       rev = "9f35c4b09fd44a77227e79ff0c1b4b6a69dff533";
   });
-in
-  with import <nixpkgs> (
+  pkgs = import <nixpkgs> (
     { overlays = [ mozilla ]; }
   );
-
-  mkShell {
-   buildInputs = [ latest.rustChannels.stable.rust rustracer ];
-   RUST_SRC_PATH = "${latest.rustChannels.stable.rust-src}/lib/rustlib/src/rust/src";
+  rustChannel = pkgs.latest.rustChannels.stable;
+in
+  with pkgs; mkShell {
+   buildInputs = [
+     (rustChannel.rust.override { extensions = [ "clippy-preview" ]; })
+   ];
+   RUST_SRC_PATH = "${rustChannel.rust-src}/lib/rustlib/src/rust/src";
   }
