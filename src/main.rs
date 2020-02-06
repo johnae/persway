e6f1690b6047a34f64bee828ef8bd4aaa2382bd4
John Axel Eriksson <john@insane.se>
2020-02-06 21:08:48 +0100

    Switch to async/await and swayipc. Add dynamic workspace naming.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,11 @@
-use derive_more::{Display, From};
-use i3ipc::{
-    event::{inner::WindowChange, Event},
-    reply::Command,
-    I3Connection, I3EventListener, Subscription,
-};
 use signal_hook::{iterator::Signals, SIGHUP, SIGINT, SIGQUIT, SIGTERM};
 use std::{process::exit, thread};
 use structopt::StructOpt;
-
-#[derive(Debug, Display, From)]
-enum PerswayError {
-    I3Msg(i3ipc::MessageError),
-    I3Establish(i3ipc::EstablishError),
-    Io(std::io::Error),
-}
-
-impl std::error::Error for PerswayError {}
-
-type Result<T> = std::result::Result<T, PerswayError>;
+use swayipc::async_std;
+use swayipc::async_std::stream::StreamExt;
+use swayipc::reply::Event;
+use swayipc::reply::{WindowChange, Workspace};
+use swayipc::{Connection, EventType, Fallible};
 
 #[derive(StructOpt)]
 struct Cli {
@@ -25,40 +13,56 @@ struct Cli {
     opacity: f64,
 }
 
-fn update_opacity(ipc: &mut I3Connection, opacity: f64) -> Result<Command> {
-    let cmd = format!("[tiling] opacity {}; opacity 1", opacity);
-    Ok(ipc.run_command(&cmd)?)
-}
-
-fn reset_opacity(ipc: &mut I3Connection) -> Result<Command> {
-    Ok(ipc.run_command("[tiling] opacity 1")?)
-}
-
-fn handle_signals() -> Result<()> {
-    let mut conn = I3Connection::connect()?;
-    let signals = Signals::new(&[SIGHUP, SIGINT, SIGQUIT, SIGTERM])?;
+fn handle_signals() {
+    let signals = Signals::new(&[SIGHUP, SIGINT, SIGQUIT, SIGTERM]).unwrap();
     signals.forever().next();
-    reset_opacity(&mut conn)?;
+    async_std::task::block_on(async {
+        let mut commands = Connection::new().await.unwrap();
+        commands.run_command("[tiling] opacity 1").await.unwrap();
+    });
     exit(0)
 }
 
-fn main() -> Result<()> {
+async fn get_focused_workspace(conn: &mut Connection) -> Result<Workspace, swayipc::Error> {
+    let mut ws = conn.get_workspaces().await?.into_iter();
+    let focused = ws.find(|w| w.focused).unwrap();
+    Ok(focused)
+}
+
+#[async_std::main]
+async fn main() -> Fallible<()> {
     let args = Cli::from_args();
     thread::spawn(handle_signals);
-    let mut conn = I3Connection::connect()?;
-    let mut listener = I3EventListener::connect()?;
-    listener.subscribe(&[Subscription::Window])?;
 
-    reset_opacity(&mut conn)?;
-    for event in listener.listen() {
+    let subscriptions = [EventType::Window];
+    let mut events = Connection::new().await?.subscribe(&subscriptions).await?;
+    let mut commands = Connection::new().await?;
+
+    while let Some(event) = events.next().await {
         match event? {
-            Event::WindowEvent(info) => {
-                if let WindowChange::Focus = info.change {
-                    update_opacity(&mut conn, args.opacity)?;
+            Event::Window(wevent) => match wevent.change {
+                WindowChange::Focus => {
+                    let cmd = format!("[tiling] opacity {}; opacity 1", args.opacity);
+                    commands.run_command(&cmd).await?;
+                    let app_id = wevent.container.app_id;
+                    let window_properties = wevent.container.window_properties;
+                    let app_name = app_id.unwrap_or_else(|| {
+                        window_properties
+                            .map(|props| props.class)
+                            .or(Some("Unknown".to_string()))
+                            .unwrap()
+                    });
+                    let current_ws = get_focused_workspace(&mut commands).await?;
+                    let num = current_ws.name.split(": ").next().unwrap();
+                    let newname = format!("{}: {}", num, app_name.to_lowercase());
+                    let cmd = format!("rename workspace to {}", newname);
+                    commands.run_command(&cmd).await?;
                 }
-            }
+                _ => {}
+            },
             _ => unreachable!(),
         }
     }
-    Ok(())
+
+    unreachable!();
 }
