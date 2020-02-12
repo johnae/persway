b01baa1f7e4460f1e5b3b5ce57172bf0e10f8a5f
John Axel Eriksson <john@insane.se>
2020-02-12 11:39:35 +0100

    shell.nix improvements.

--- shell.nix
+++ shell.nix
@@ -1,17 +1,22 @@
 let
   mozilla = import (builtins.fetchGit {
-      url = "https://github.com/mozilla/nixpkgs-mozilla.git";
-      ref = "master";
-      rev = "9f35c4b09fd44a77227e79ff0c1b4b6a69dff533";
+    url = "https://github.com/mozilla/nixpkgs-mozilla.git";
+    ref = "master";
+    rev = "36455d54de0b40d9432bba6d8207a5582210b3eb";
   });
-  pkgs = import <nixpkgs> (
-    { overlays = [ mozilla ]; }
-  );
+
+  pkgs = import <nixpkgs> ({ overlays = [ mozilla ]; });
+
   rustChannel = pkgs.latest.rustChannels.stable;
-in
-  with pkgs; mkShell {
-   buildInputs = [
-     (rustChannel.rust.override { extensions = [ "clippy-preview" ]; })
-   ];
-   RUST_SRC_PATH = "${rustChannel.rust-src}/lib/rustlib/src/rust/src";
-  }
+
+  rust = rustChannel.rust.override {
+    extensions = [ "rust-src" "clippy-preview" "rustfmt-preview" ];
+  };
+
+  cargo = rustChannel.cargo;
+
+in with pkgs;
+mkShell {
+  buildInputs = [ rust cargo ];
+  RUST_SRC_PATH = "${rustChannel.rust-src}/lib/rustlib/src/rust/src";
+}
