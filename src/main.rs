72c58e9edcb87d8dfe418a746454583069a0f50c
John Axel Eriksson <john@insane.se>
2020-02-26 22:43:18 +0100

    Implement autolayout / autotiling.

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ use structopt::StructOpt;
 use swayipc::async_std;
 use swayipc::async_std::stream::StreamExt;
 use swayipc::reply::Event;
-use swayipc::reply::{WindowChange, Workspace};
+use swayipc::reply::{Node, NodeLayout, NodeType, WindowChange, Workspace};
 use swayipc::{Connection, EventType, Fallible};
 
 #[derive(StructOpt)]
 struct Cli {
     #[structopt(short = "o", long = "opacity", default_value = "0.78")]
     opacity: f64,
+    #[structopt(short = "a", long = "autolayout")]
+    autolayout: bool,
 }
 
 fn handle_signals() {
@@ -21,6 +23,30 @@ fn handle_signals() {
         commands.run_command("[tiling] opacity 1").await.unwrap();
     });
     exit(0)
+}
+
+async fn autolayout(conn: &mut Connection) -> Fallible<()> {
+    let tree = conn.get_tree().await?;
+    let focused: &Node = tree
+        .find_focused_as_ref(|n| n.focused)
+        .expect("Expected a node to be focused");
+    let parent: &Node = tree
+        .find_focused_as_ref(|n| n.nodes.iter().any(|n| n.focused))
+        .expect("Expected to find a parent");
+    let is_floating = focused.node_type == NodeType::FloatingCon;
+    let is_full_screen = focused.percent.unwrap_or(1.0) > 1.0;
+    let is_stacked = parent.layout == NodeLayout::Stacked;
+    let is_tabbed = parent.layout == NodeLayout::Tabbed;
+    let change_split = !is_floating && !is_full_screen && !is_stacked && !is_tabbed;
+    if change_split {
+        if focused.rect.height > focused.rect.width {
+            conn.run_command("split v").await?;
+        } else {
+            conn.run_command("split h").await?;
+        }
+    };
+
+    Ok(())
 }
 
 async fn get_focused_workspace(conn: &mut Connection) -> Fallible<Workspace> {
@@ -45,6 +71,7 @@ async fn main() -> Fallible<()> {
                 WindowChange::Focus => {
                     let cmd = format!("[tiling] opacity {}; opacity 1", args.opacity);
                     commands.run_command(&cmd).await?;
+
                     let app_id = wevent.container.app_id;
                     let window_properties = wevent.container.window_properties;
                     let app_name = app_id.unwrap_or_else(|| {
@@ -62,6 +89,9 @@ async fn main() -> Fallible<()> {
                     let newname = format!("{}: {}", num, app_name.to_lowercase());
                     let cmd = format!("rename workspace to {}", newname);
                     commands.run_command(&cmd).await?;
+                    if args.autolayout {
+                        autolayout(&mut commands).await?;
+                    };
                 }
                 _ => {}
             },
