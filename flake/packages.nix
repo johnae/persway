954e5b962681f5e0b78124cc0570f5153e6e9859
John Axel Eriksson <john@insane.se>
2024-09-11 22:12:36 +0200

    chore: update flake/crane + fix deprecation

--- flake/packages.nix
+++ flake/packages.nix
@@ -4,9 +4,10 @@
     pkgs,
     ...
   }: let
-    craneLib =
-      inputs.crane.lib.${system}.overrideToolchain
-      inputs.fenix.packages.${system}.minimal.toolchain;
+    craneLib = inputs.crane.mkLib pkgs;
+    # craneLib =
+    #   inputs.crane.lib.${system}.overrideToolchain
+    #   inputs.fenix.packages.${system}.minimal.toolchain;
   in {
     packages.default = craneLib.buildPackage {
       src = craneLib.cleanCargoSource (craneLib.path ../.);
